6c69dab70300920e35068067d68a880b8b4f987b
Dark-free <emchaouki@gmail.com>
2021-09-02 14:44:26 +0100

    cherish: adapt for cherish to tests

--- cherish_cepheus.mk
+++ cherish_cepheus.mk
@@ -21,21 +21,37 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from cepheus device
 $(call inherit-product, device/xiaomi/cepheus/device.mk)
 
-# Inherit some common PPUI stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common CHERISH stuff.
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# FOD animations
+EXTRA_FOD_ANIMATIONS := true
+TARGET_WANTS_FOD_ANIMATIONS := true
 
 # Device identifier
-PRODUCT_NAME := aosp_cepheus
+PRODUCT_NAME := cherish_cepheus
 PRODUCT_DEVICE := cepheus
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9
 PRODUCT_MANUFACTURER := Xiaomi
+CHERISH_BUILD_TYPE := OFFICIAL
+WITH_GMS := true
 
-# PixelPlus Properties !
-TARGET_INCLUDE_WIFI_EXT := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_FACE_UNLOCK_SUPPORTED = true
-CUSTOM_BUILD_TYPE=OFFICIAL
+PRODUCT_GENERIC_PROPERTIES += \
+    ro.cherish.maintainer=Freesoul00
+
+	# Build Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+TARGET_DEVICE="cepheus" \
+PRODUCT_NAME="cepheus" \
+PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210605.003 7255357 release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys"
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+		ro.build.fingerprint=$(BUILD_FINGERPRINT)
 
 # Include firmware
 $(call inherit-product, vendor/xiaomi/firmware/cepheus/firmware.mk)
