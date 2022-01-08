f7bea4cb3868d6fb12ecb2292367eb770a068270
dark-free <emchaouki@gmail.com>
2022-01-08 21:18:50 +0000

    Revert "fingerprint: clean up BiometricsFingerprint"
    This reverts commit 804ce809090a07a26de9221e6f1ede98fcc1c810.

--- fingerprint/BiometricsFingerprint.h
+++ fingerprint/BiometricsFingerprint.h
@@ -18,15 +18,15 @@
 #ifndef ANDROID_HARDWARE_BIOMETRICS_FINGERPRINT_V2_3_BIOMETRICSFINGERPRINT_H
 #define ANDROID_HARDWARE_BIOMETRICS_FINGERPRINT_V2_3_BIOMETRICSFINGERPRINT_H
 
-
-#ifdef ENABLE_UDFPS
-#include <android-base/unique_fd.h>
-#include "hardware/fingerprint.h"
-#else
-#include <hardware/fingerprint.h>
-#endif
-
 #include <android/hardware/biometrics/fingerprint/2.3/IBiometricsFingerprint.h>
+#include <android/log.h>
+#include <hardware/hardware.h>
+#include <hidl/MQDescriptor.h>
+#include <hidl/Status.h>
+#include <log/log.h>
+#include <vendor/xiaomi/hardware/fingerprintextension/1.0/IXiaomiFingerprint.h>
+
+#include "fingerprint.h"
 
 namespace android {
 namespace hardware {
@@ -46,18 +46,20 @@ using ::android::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprint
 using ::android::hardware::biometrics::fingerprint::V2_1::RequestStatus;
 using ::android::hardware::biometrics::fingerprint::V2_3::IBiometricsFingerprint;
 
-struct BiometricsFingerprint : public IBiometricsFingerprint {
-  public:
+using ::vendor::xiaomi::hardware::fingerprintextension::V1_0::IXiaomiFingerprint;
+
+struct BiometricsFingerprint : public IBiometricsFingerprint, public IXiaomiFingerprint {
     BiometricsFingerprint();
     ~BiometricsFingerprint();
+
+    status_t registerAsSystemService();
 
     // Method to wrap legacy HAL with BiometricsFingerprint class
     static IBiometricsFingerprint* getInstance();
 
-    // Methods from ::android::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprint
-    // follow.
+    // Methods from ::android::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprint follow.
     Return<uint64_t> setNotify(
-            const sp<IBiometricsFingerprintClientCallback>& clientCallback) override;
+        const sp<IBiometricsFingerprintClientCallback>& clientCallback) override;
     Return<uint64_t> preEnroll() override;
     Return<RequestStatus> enroll(const hidl_array<uint8_t, 69>& hat, uint32_t gid,
                                  uint32_t timeoutSec) override;
@@ -68,14 +70,12 @@ struct BiometricsFingerprint : public IBiometricsFingerprint {
     Return<RequestStatus> remove(uint32_t gid, uint32_t fid) override;
     Return<RequestStatus> setActiveGroup(uint32_t gid, const hidl_string& storePath) override;
     Return<RequestStatus> authenticate(uint64_t operationId, uint32_t gid) override;
-    Return<bool> isUdfps(uint32_t sensorId) override;
-    Return<void> onFingerDown(uint32_t x, uint32_t y, float minor, float major) override;
-    Return<void> onFingerUp() override;
 
-  private:
+    Return<int32_t> extCmd(int32_t cmd, int32_t param) override;
+
     static fingerprint_device_t* openHal();
     static void notify(
-            const fingerprint_msg_t* msg); /* Static callback for legacy HAL implementation */
+        const fingerprint_msg_t* msg); /* Static callback for legacy HAL implementation */
     static Return<RequestStatus> ErrorFilter(int32_t error);
     static FingerprintError VendorErrorFilter(int32_t error, int32_t* vendorCode);
     static FingerprintAcquiredInfo VendorAcquiredFilter(int32_t error, int32_t* vendorCode);
@@ -85,9 +85,10 @@ struct BiometricsFingerprint : public IBiometricsFingerprint {
     sp<IBiometricsFingerprintClientCallback> mClientCallback;
     fingerprint_device_t* mDevice;
 
-#ifdef ENABLE_UDFPS
-    android::base::unique_fd touch_fd_;
-#endif
+    // Methods from ::android::hardware::biometrics::fingerprint::V2_3::IBiometricsFingerprint follow.
+    Return<bool> isUdfps(uint32_t sensorId) override;
+    Return<void> onFingerDown(uint32_t x, uint32_t y, float minor, float major) override;
+    Return<void> onFingerUp() override;
 };
 
 }  // namespace implementation
